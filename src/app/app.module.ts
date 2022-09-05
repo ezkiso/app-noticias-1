4db356fc639b5f94e25b276c4889e499947cde0f
ezkiso <bastian1lobos@gmail.com>
2022-09-05 16:08:39 -0300

    terceraactualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-  import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 
 @NgModule({
@@ -16,7 +15,8 @@ import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    IonicStorageModule.forRoot()
   ],
   providers: [{ provide: RouteReuseStrategy,useClass: IonicRouteStrategy },
     InAppBrowser
--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
+import{Share} from '@capacitor/share';
+import { StorageService } from 'src/app/services/storage.service';
 
 
 @Component({
@@ -13,7 +16,9 @@ export class ArticleComponent implements OnInit {
   @Input() article:Article;
   @Input() index:number;
 
-  constructor(private iab:InAppBrowser) { }
+  constructor(private iab:InAppBrowser,
+              private actionSheetCtrl:ActionSheetController,
+              private storageService:StorageService){}
 
   ngOnInit() {}
   openArticle() {
@@ -21,5 +26,46 @@ export class ArticleComponent implements OnInit {
     const browser =this.iab.create(this.article.url);
     browser.show();
   }
-
+  async openMenu()
+  {
+    const actionSheet = await this.actionSheetCtrl.create( 
+      {
+        header:'options',
+        buttons: [
+          {
+            text:'Share',
+            icon:'share-outline',
+            handler:()=>this.shareArticle()
+          },
+          {
+            text:'Favorites',
+            icon:'heart-outline',
+            handler:()=>this.onToggleFavorite()
+          },
+          {
+            text:'Cancel',
+            icon:'close-outline',
+            role:'cancel'
+          }
+          
+        ]
+        
+      }
+    );
+    await actionSheet.present();
+  }
+  async shareArticle()
+  { 
+    await Share.share({
+      title:this.article.title,
+      text:this.article.source.name,
+      url:this.article.url
+    });
+  }
+  async onToggleFavorite()
+  {
+    console.log('favoritos')
+    await this.storageService.saveOrRemoveArticle(this.article);
+    
+  }
 }
